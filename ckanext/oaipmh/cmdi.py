dd2e8a2219a0ab7207a658aefe63fbae669cf9dd
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-08-25 12:36:06 +0300

    CSCETSIN-57: Simplify code by moving some data catalog logic to ckanext-etsin

--- ckanext/oaipmh/cmdi.py
+++ ckanext/oaipmh/cmdi.py
@@ -9,7 +9,7 @@ from ckanext.oaipmh import importformats
 from ckanext.oaipmh.cmdi_reader import CmdiReader
 from ckanext.oaipmh.harvester import OAIPMHHarvester
 
-from ckanext.etsin.data_catalog_service import DataCatalogMetaxAPIService
+from ckanext.etsin.data_catalog_service import get_data_catalog_id
 
 log = logging.getLogger(__name__)
 
@@ -17,8 +17,6 @@ log = logging.getLogger(__name__)
 class CMDIHarvester(OAIPMHHarvester):
     md_format = 'cmdi0571'
     client = None  # used for testing
-
-    DATA_CATALOG_JSON_FILE_PATH = "resources/language_bank_data_catalog.json"
 
     def info(self):
         ''' See ;meth:`ckanext.harvest.harvesters.base.HarvesterBase.info`. '''
@@ -48,10 +46,6 @@ class CMDIHarvester(OAIPMHHarvester):
     def gather_stage(self, harvest_job):
         """ See :meth:`OAIPMHHarvester.gather_stage`  """
 
-        # Get data catalog id to be used in harvest_object
-        # so that import_stage can access it
-        catalog_id = self._get_data_catalog_id()
-
         config = self._get_configuration(harvest_job)
         if not config.get('type'):
             config['type'] = 'cmdi'
@@ -59,11 +53,10 @@ class CMDIHarvester(OAIPMHHarvester):
             harvest_job.source.save()
         registry = self.metadata_registry(config, harvest_job)
         client = self.client or oaipmh.client.Client(harvest_job.source.url, registry)
-        return self.populate_harvest_job(harvest_job, None, config, client, catalog_id)
+
+        # Get data catalog id to be used in harvest_object
+        # so that import_stage can access it
+        return self.populate_harvest_job(harvest_job, None, config, client, get_data_catalog_id(config))
 
     def parse_xml(self, f, context, orig_url=None, strict=True):
         return CmdiReader().read_data(etree.fromstring(f))
-
-    def _get_data_catalog_id(self):
-        catalog_service = DataCatalogMetaxAPIService()
-        return catalog_service.create_or_update_data_catalogs(True, CMDIHarvester.DATA_CATALOG_JSON_FILE_PATH)
