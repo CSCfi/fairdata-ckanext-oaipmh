176329d6082e2d8d567929a5efefdc3d6e59a8c6
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2018-03-12 17:57:00 +0200

    CSCETSIN-18: [FIX] Fix

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(
         [ckan.plugins]
         cmdi_harvester=ckanext.oaipmh.cmdi:CMDIHarvester
         datacite_harvester=ckanext.oaipmh.datacite:DataCiteHarvester
-        ddi_harvester=ckanext.oaipmh.ddi25:Ddi25Harvester
+        ddi25_harvester=ckanext.oaipmh.ddi25:Ddi25Harvester
         """,
 )
